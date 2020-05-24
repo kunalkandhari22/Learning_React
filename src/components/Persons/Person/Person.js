21366ca667a675ec98c2aa0fd54bc26111f427eb
kunalkandhari22 <kunalkandhari22@gmail.com>
2020-05-24 20:07:56 +0530

    Changed functions to classes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import style from './Person.module.css';
 
-const Person = (props) => {
-
-    return (
-        <div className={style.Person}>
-            <p onClick={props.click}>My name is {props.name} and I'm {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-            <h3 className={style.heading}>Heading</h3>
-        </div>
-    )
+class Person extends Component {
+    render() {
+        return (
+                <div className={style.Person}>
+                <p onClick={this.props.click}>My name is {this.props.name} and I'm {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <h3 className={style.heading}>Heading</h3>
+                </div>
+            )
+    }
 }
+
+// const Person = (props) => {
+
+//     return (
+//         <div className={style.Person}>
+//             <p onClick={props.click}>My name is {props.name} and I'm {props.age} years old</p>
+//             <p>{props.children}</p>
+//             <input type="text" onChange={props.changed} value={props.name} />
+//             <h3 className={style.heading}>Heading</h3>
+//         </div>
+//     )
+// }
 
 export default Person;
